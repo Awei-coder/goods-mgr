09f7f4f3a71865fd8b5caf0f489e758bbfe4343b
AweiCoder <wcWang1013@outlook.com>
2021-03-04 21:48:01 +0800

    验证页面初始化

---
+++ goods-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,14 @@
+import { defineComponent } from 'vue'
+import { UserOutlined, LockOutlined,  MessageOutlined} from '@ant-design/icons-vue'
+
+export default defineComponent({
+  setup() {
+
+  },
+  components: {
+    // 图标组件注册
+    UserOutlined,
+    LockOutlined,
+    MessageOutlined
+  }
+});
